d022bd7181743f38162b331b25b8bf63ddce670b
intan khalimatu <i.2510261@unida.ac.id>
2025-11-09 01:55:22 +0700

    UTSALPRO

---
+++ Variabel.java
@@ -0,0 +1,21 @@
+public class Variabel {
+    public static void main(String[]args) {
+        String salam = "Assalamu'alaikum";
+        String Perkenalan = "Izinkan saya memperkenalkan diri";
+        String nama = "Intan Khalimatu Sa'diyah";
+        String tempattanggallahir = "Sukabumi, 31 desember 2006";
+        String jeniskelamin = "Perempuan";
+        String alamat = "Mega residence blok a, Cikahuripan, Klapanunggal, bogor";
+        String nohp = "0895-0619-3727";
+        String penghasilan = "Rp.100.000.000";
+
+        System.out.println(salam);
+        System.out.println(Perkenalan);
+        System.out.println("Nama Lengkap           : "+ nama);
+        System.out.println("Tempat & Tanggal Lahir : "+ tempattanggallahir);
+        System.out.println("Jenis Kelamin          : "+ jeniskelamin);
+        System.out.println("Alamat Lengkap         : "+ alamat);
+        System.out.println("No Hp                  :"+ nohp);
+        System.out.println("Penghasilan Sebulan    : "+ penghasilan);
+    }
+}
